419ca5f0f6ea355a6b505cff2d6f97e18a7535dd
unknown <esierrac@choucairtesting.com>
2024-07-31 17:29:40 -0500

    v1

--- curso-midu-metacritic/App.js
+++ curso-midu-metacritic/App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { getLatestGames } from './lib/metacritics';
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+  });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <SafeAreaView>
+      <ScrollView>
+        {games.map((game) => (
+          <View key={game.slug} style={styles.card}>
+            <Image source={{ uri: game.image }} style={styles.image}/>
+            <Text style={styles.title}>{game.title}</Text>
+            <Text style={styles.score}>{game.score}</Text>
+            <Text style={styles.description}>{game.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -16,5 +38,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginRight: 20,
+    marginLeft: 20
   },
+  card: {
+    marginBottom: 42,
+    alignItems: 'center'
+  },
+  image: {
+    width: 127,
+    height: 167,
+    borderRadius: 10
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+    marginTop: 10,
+    fontFamily: ''
+  },
+  score: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'green',
+    marginBottom: 10
+  },
+  description: {
+    fontSize: 16,
+    color: '#000'
+  }
 });
